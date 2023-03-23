67d5abea045886900a0dab4fa0d72efae8da8e46
Irène Duménil <126900447+idumenil@users.noreply.github.com>
2023-03-23 16:15:42 +0100

    Update ft_printhex.c

--- ft_printhex.c
+++ ft_printhex.c
@@ -1,52 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printhex.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idumenil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/15 15:17:15 by idumenil          #+#    #+#             */
+/*   Updated: 2023/03/15 15:21:20 by idumenil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-void    ft_putchar(char c)
+int	ft_hexlen(unsigned int nbr)
 {
-    write(1, &c, 1);
+	int	i;
+
+	i = 0;
+	while (nbr != 0)
+	{
+		nbr = nbr / 16;
+		i++;
+	}
+	return (i);
 }
 
-int ft_hexlen(unsigned int nbr)
+void	ft_puthex(unsigned int nbr, const char format)
 {
-    int i;
-
-    i = 0;
-    while (nbr != 0)
-    {
-        nbr = nbr / 16;
-        i++;
-    }
-    return (i);
+	if (nbr >= 16)
+	{
+		ft_puthex(nbr / 16, format);
+		ft_puthex(nbr % 16, format);
+	}
+	else
+	{
+		if (nbr > 9)
+		{
+			if (format == 'x')
+				ft_putchar_fd(nbr - 10 + 'a', 1);
+			if (format == 'X')
+				ft_putchar_fd(nbr - 10 + 'A', 1);
+		}
+		else
+			ft_putchar_fd(nbr + '0', 1);
+	}
 }
 
-void    ft_puthex(unsigned int nbr, const char format)
+int	ft_printhex(unsigned int nbr, const char format)
 {
-    if (nbr >= 16)
-    {
-        ft_puthex(nbr / 16, format);
-        ft_puthex(nbr % 16, format);
-    }
-    else
-    {
-        if (nbr > 9)
-        {
-            if (format == 'x')
-                ft_putchar(nbr - 10 + 'a');
-            if (format == 'X')
-                ft_putchar(nbr - 10 + 'A');
-        }
-        else
-            ft_putchar(nbr + '0');
-    }
-}
-
-int ft_printhex(unsigned int nbr, const char format)
-{
-    if (nbr == 0)
-    {
-        ft_putchar('0');
-        return (1);
-    }
-    else
-        ft_puthex(nbr, format);
-    return (ft_hexlen(nbr));
+	if (nbr == 0)
+	{
+		ft_putchar_fd('0', 1);
+		return (1);
+	}
+	else
+		ft_puthex(nbr, format);
+	return (ft_hexlen(nbr));
 }
