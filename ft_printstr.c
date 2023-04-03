cca8d65edfc3373618d05f06487a478d5eff09e1
Irène Duménil <126900447+idumenil@users.noreply.github.com>
2023-04-03 13:07:22 +0200

    Update ft_printstr.c

--- ft_printstr.c
+++ ft_printstr.c
@@ -1,19 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printstr.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idumenil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/15 14:52:14 by idumenil          #+#    #+#             */
+/*   Updated: 2023/03/15 14:57:17 by idumenil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-int ft_printstr(const char *str)
+int	ft_printstr(const char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if (str == NULL)
-    {
-        write(1, "(null)", 6);
-        return (6);
-    }
-    while (str[i])
-    {
-        write(1, &str[i], 1);
-        i++;
-    }
-    return (i);
+	i = 0;
+	if (str == NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	return (i);
 }
