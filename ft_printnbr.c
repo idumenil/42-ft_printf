74e9a644ed4f7ebecdf7646716b394216f1e873b
Irène Duménil <126900447+idumenil@users.noreply.github.com>
2023-04-03 13:06:30 +0200

    Update ft_printnbr.c

--- ft_printnbr.c
+++ ft_printnbr.c
@@ -1,16 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printnbr.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idumenil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/15 14:59:12 by idumenil          #+#    #+#             */
+/*   Updated: 2023/03/15 15:29:58 by idumenil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
-#include "libft/libft.h"
 
-int ft_printnbr(int n)
+int	ft_printnbr(int n)
 {
-    int len;
-    char    *char_n;
+	int		len;
+	char	*char_n;
 
-    len = 0;
-    char_n = ft_itoa(n);
-    len = ft_printstr(char_n);
-    free(char_n);
-    return (len);
+	len = 0;
+	char_n = ft_itoa(n);
+	len = ft_printstr(char_n);
+	free(char_n);
+	return (len);
 }
-
-
